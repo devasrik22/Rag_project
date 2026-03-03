8a73cc70c592d02f3fe70bb9fecdafd78b69f5da
Devasri K <devasridkk@gmail.com>
2026-03-03 11:44:36 +0530

    Updated project files

--- eg.py
+++ eg.py
@@ -1,49 +1,23 @@
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
-
-import os
-import shutil
-
-# Startup Cleanup
-if os.path.exists("./chroma_db"):
-    shutil.rmtree("./chroma_db")
-    print("Old vector database removed.")
-
-# 1️⃣ Load PDFs
-loader = DirectoryLoader(
-    path="data",
-    glob="*.pdf",
-    loader_cls=PyPDFLoader
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+load_dotenv()
+llm = ChatGroq(
+    model='llama-3.1-8b-instant',
+    temperature=0.7
 )
 
-documents = loader.load()
-print("Loaded documents:", len(documents))
 
-# 2️⃣ Split Text
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=3000,
-    chunk_overlap=200
-)
+#1st prompts for detailed report 
+template1 = PromptTemplate(template="write a detailed report on the {topic}",
+                           input_variables=['topic'])
 
-split_docs = text_splitter.split_documents(documents)
-print("After splitting:", len(split_docs))
+#2nd prompt for a 5 line summary 
+template2 = PromptTemplate(template="write a 5 line summary on the {text}",
+                           input_variables=['text'])
 
-for doc in split_docs:
-    doc.metadata = {}
-    
-# 3️⃣ Create Embeddings
-embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+prompt1 = template1.invoke({'topic': "Black hole"})
 
-# 4️⃣ Store in Chroma Vector DB
-vector_store = Chroma.from_documents(
-    documents=split_docs,
-    embedding=embedding_model,
-    persist_directory="./chroma_db",
-    collection_name="fresh_session"
-)
-vector_store.persist()
-print("Vector database created successfully!")
+response = llm.invoke(prompt1)
+
+print(response.content)
